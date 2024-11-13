607db8685f69a85ef7c465558c16f8fb533b985d
Chetan Kalal <kalalchetan77@gmail.com>
2024-11-13 17:36:20 +0530

    React-1_Intro_React_And_Install

--- REACT-1_Intro_of_ReactJS_And_Install.js
+++ REACT-1_Intro_of_ReactJS_And_Install.js
@@ -152,6 +152,6 @@ II.git clone(git regular downloads )
 -->Add a file using 'git add .\REACT-1_Intro_of_ReactJS_And_Install.js'
 -->The commit the the code using 'git commit -m "React-1_Intro_React_And_Install"'
 -->After the check the branch using 'git branch' it's a 'main'
--->Then push your docment using 'git push origin main'
+-->Then push your doc on github using 'git push origin main'
 
 */
