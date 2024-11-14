8f6bce8d6cce309e91babd53e9dcf257594d441a
Chetan Kalal <kalalchetan77@gmail.com>
2024-11-14 16:29:33 +0530

    React-2_React_Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+//This below function are react component
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+
+//This react 18.3.1 version - import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//This react 17 version - import ReactDOM from 'react-dom';
+// ReactDOM.render(<App />, document.getElementById('root'));
+
